30c342b778bb62cee996e4173208be66e99219f0
ocowchun <ocowchun@gmail.com>
2016-02-18 13:22:46 +0800

    adjust metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,10 @@ name "simple_consul_alerts"
 version "0.0.2"
 maintainer "ocowchun"
 maintainer_email "ocowchun@gmail.com"
+license  "MIT"
 supports "ubuntu"
+description "install consul alert"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 
 depends 'apt'
 depends 'golang'
